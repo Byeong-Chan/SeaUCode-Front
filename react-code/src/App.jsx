0cd73abf31228b9a111fa25850fa2944af0ba54d
qudcksquf <SeoByeongChan@seobyeongchan-ui-MacBook-Pro.local>
2019-10-11 04:59:45 +0900

    implementation Loged in

--- react-code/src/App.jsx
+++ react-code/src/App.jsx
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import 'typescript';
 import ResisterForm from './component/RegisterForm';
 import LoginForm from './component/LoginForm';
+import UserUI from './component/UserUI';
+import { useDispatch } from 'react-redux';
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
+
+const setToken = refresh_token => ({ type: "token/SET_TOKEN", refresh_token });
+
+function Greeting(props) {
+    if(props.isLogedIn) {
+        return <UserUI />;
+    }
+    else {
+        return <LoginForm />;
+    }
+}
 
 function App() {
+
+    const [isLogedIn, toggleLogedIn] = useState(false);
+
+    const [cookies] = useCookies(['access_token']);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        async function cookie_update() {
+            const refresh_token = cookies.access_token || '';
+            dispatch(setToken(refresh_token));
+
+            axios.defaults.baseURL = 'http://127.0.0.1:3000';
+            axios.defaults.headers.common['x-access-token'] = refresh_token;
+            axios.get('/logintest').then(response => { // TODO: logintest 가 아니라 유저정보 가져오는 쿼리를 쓸것
+                //TODO: redux에 유저정보 저장하고 시작할 것
+                toggleLogedIn(true);
+            }).catch(err => {
+                toggleLogedIn(false);
+            });
+        };
+        cookie_update();
+    }, [cookies, dispatch]);
+
     return (
         <div className="App">
             <ResisterForm/>
-            <LoginForm/>
+            <Greeting isLogedIn={isLogedIn}/>
         </div>
     );
 }
--- react-code/src/component/LoginForm.jsx
+++ react-code/src/component/LoginForm.jsx
@@ -1,30 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import 'typescript';
 import axios from 'axios';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useCookies } from 'react-cookie';
 
-const clearToken = () => ({ type: "token/CLEAR_TOKEN" });
 const setToken = refresh_token => ({ type: "token/SET_TOKEN", refresh_token });
 
 function LoginForm() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [cookies, setCookie] = useCookies(['access_token']);
-
-    const access_token = useSelector(
-        state => state.token
-    );
+    const [Cookie, setCookie, removeCookie] = useCookies(['access_token']);
 
     const dispatch = useDispatch();
-
-    useEffect(() => {
-        async function cookie_update() {
-            const refresh_token = cookies.access_token || '';
-            dispatch(setToken(refresh_token));
-        };
-        cookie_update();
-    }, [cookies, dispatch]);
 
     const changeEmail = e => {
         setEmail(e.target.value);
@@ -39,6 +26,7 @@ function LoginForm() {
             dispatch(setToken(response.data.token));
             setCookie('access_token', response.data.token, { maxAge: 60*60*24*7 });
         }).catch(err => {
+            //TODO: 로그인 실패 했을 때 구현 해주세요.
             console.log(err);
         });
     };
@@ -46,7 +34,7 @@ function LoginForm() {
     return (
         <div className="LoginForm">
             <br/>
-            <input value={email} placeholder="이메일" onChange={changeEmail}/>
+            < input value={email} placeholder="이메일" onChange={changeEmail}/>
             <br/>
             <input type="password" value={password} placeholder="비밀번호" onChange={changePassword}/>
             <br/>

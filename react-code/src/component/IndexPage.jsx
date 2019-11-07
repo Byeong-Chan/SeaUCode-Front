d65db7c3195236aa2120c364e23321945925246b
qudcksquf <SeoByeongChan@seobyeongchan-ui-MacBook-Pro.local>
2019-11-07 18:56:45 +0900

    create index page

---
+++ react-code/src/component/IndexPage.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import 'typescript';
+import { Button, Form, Modal, Col, Row, Carousel } from 'react-bootstrap';
+
+function CreateClass() {
+
+    //const [isLogedIn, toggleLogedIn] = useState(false);
+
+    return (
+        <div className="CreateClass">
+            index page
+        </div>
+    );
+}
+
+export default CreateClass;

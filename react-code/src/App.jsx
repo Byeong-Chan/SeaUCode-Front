e0512b4b8bc68321e5a0aebcd8e44d6f53840c2a
nylee9621 <fnql9621@daum.net>
2019-11-07 20:27:29 +0900

    홈화면 슬라이드 추가, 로그인시 엔터키 기능 추가, 마이페이지 제작중

--- react-code/src/App.jsx
+++ react-code/src/App.jsx
@@ -11,7 +11,7 @@ import CreateClass from './component/CreateClass';
 import { useDispatch } from 'react-redux';
 import { useCookies } from 'react-cookie';
 import axios from 'axios';
-import { Button, ButtonToolbar, Navbar, Nav} from 'react-bootstrap';
+import { Button, ButtonToolbar, Navbar, Nav, Carousel} from 'react-bootstrap';
 import {
     BrowserRouter as Router,
     Switch,
@@ -111,6 +111,22 @@ function App() {
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
+
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://cdn2.slidemodel.com/wp-content/uploads/FF0215-01-free-web-developer-icons-for-powerpoint-16x9-1.jpg"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://cdn2.slidemodel.com/wp-content/uploads/FF0215-01-free-web-developer-icons-for-powerpoint-16x9-1.jpg"
+                        />
+                    </Carousel.Item>
+                </Carousel>
+
                 <div>
                     <Switch>
                         <Route path="/createClass">
--- react-code/src/component/LoginedUserTopNav.jsx
+++ react-code/src/component/LoginedUserTopNav.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import 'typescript';
 import { useDispatch } from 'react-redux';
 import { useCookies } from 'react-cookie';
-import { Button } from 'react-bootstrap'
+import {Button, ButtonToolbar, Nav, Navbar, Modal, Form, Row, Col} from 'react-bootstrap'
 
 import {
     BrowserRouter as Router,
@@ -12,10 +12,13 @@ import {
     useRouteMatch,
     useParams
 } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import axios from "axios";
+import CreateClass from "./CreateClass";
 
 const clearToken = () => ({ type: "token/CLEAR_TOKEN" });
 
-function LoginedUserTopNav() {
+function LoginedUserTopNav(props) {
     const [Cookie, setCookie, removeCookie] = useCookies(['access_token']);
 
     const dispatch = useDispatch();
@@ -25,12 +28,79 @@ function LoginedUserTopNav() {
         removeCookie('access_token');
     };
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
     return (
         <div className="LoginedUserTopNav">
             <Link to="/"><Button onClick={logout} variant="dark">로그아웃</Button></Link>
             <Link to="/createClass"><Button variant="dark">반 만들기</Button></Link>
+
+            <Button variant="dark" onClick={handleShow}>
+                마이페이지
+            </Button>
+
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>마이페이지</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group as={Row}>
+                            <Form.Label column sm={4}>
+                                닉네임
+                            </Form.Label>
+                            <Col sm={8}>
+                                홍길동
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row}>
+                            <Form.Label column sm={4}>
+                                이메일
+                            </Form.Label>
+                            <Col sm={8}>
+                                SeaUCode@ajou.ac.kr
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row}>
+                            <Form.Label column sm={4}>
+                                비밀번호 변경
+                            </Form.Label>
+                            <Col sm={8}>
+                                <Form.Control type="password" placeholder="password" />
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row}>
+                            <Form.Label column sm={4}>
+                                비밀번호 확인
+                            </Form.Label>
+                            <Col sm={8}>
+                                <Form.Control type="password" placeholder="password" />
+                            </Col>
+                        </Form.Group>
+
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        닫기
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        수정
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
         </div>
     );
 }
+
 
 export default LoginedUserTopNav;

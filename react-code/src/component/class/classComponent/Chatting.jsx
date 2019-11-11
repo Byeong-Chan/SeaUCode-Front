b9c0fd3af31d33d485e78c280cc1135b66279850
nylee9621 <fnql9621@daum.net>
2019-11-11 20:21:28 +0900

    chatting form ing

---
+++ react-code/src/component/class/classComponent/Chatting.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import 'typescript';
+import { Button, Form, Col, Row } from 'react-bootstrap';
+
+function Chatting() {
+
+    return (
+        <div className="chatting">
+            채팅
+        </div>
+    );
+}
+
+export default Chatting;

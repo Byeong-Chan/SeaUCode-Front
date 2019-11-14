b46f4c86617c2c3a0eb2d653489242137ae72f22
qudcksquf <SeoByeongChan@seobyeongchan-ui-MacBook-Pro.local>
2019-11-14 20:55:09 +0900

    modified config for add redux function

--- react-code/src/config.js
+++ react-code/src/config.js
@@ -2,5 +2,8 @@ module.exports = {
     'serverURL' : 'http://127.0.0.1:3000',
     'TOGGLE_LOGGED_IN': 'isLoggedIn/TOGGLE_LOGGED_IN',
     'CLEAR_TOKEN' : 'token/CLEAR_TOKEN',
-    'SET_TOKEN' : 'token/SET_TOKEN'
+    'SET_TOKEN' : 'token/SET_TOKEN',
+    'SET_USER_NAME' : 'name/SET_USER_NAME',
+    'SET_USER_EMAIL': 'email/SET_USER_EMAIL',
+    'SET_USER_NICKNAME' : 'nickname/SET_USER_NICKNAME'
 };

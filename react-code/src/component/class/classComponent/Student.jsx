abcbc397e052417c2a4d265ba240d61cd52275b9
nylee9621 <fnql9621@daum.net>
2019-11-21 15:53:46 +0900

    App에서 classlist 라우트 수정, 학생 리스트에서 학생 추가 기능

--- react-code/src/component/class/classComponent/Student.jsx
+++ react-code/src/component/class/classComponent/Student.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import 'typescript';
 import { useSelector, useDispatch } from 'react-redux';
 import { useCookies } from 'react-cookie';
-import {Button, Card, Table} from 'react-bootstrap';
+import {Button, Form, Modal, Table} from 'react-bootstrap';
 import axios from 'axios';
 import config from '../../../config';
 import generalFunctions from "../../../generalFunctions";
@@ -48,10 +48,26 @@ function Student(props) {
     ]
     const tableTemplate = students.map((student) =>
         <tr key={student._id}>
-            <td>{student._id}</td>
+            <td>1</td>
             <td><Link to={'studentInfo/' + student._id}>{student.name}</Link></td>
+            <td><Button variant="danger" size="sm" onClick={postDelStudent}>탈퇴</Button></td>
         </tr>
     )
+
+    const [showAddStudent, setShowAddStudent] = useState(false);
+    const handleCloseAddStudent = () => setShowAddStudent(false);
+    const handleShowAddStudent = () => setShowAddStudent(true);
+
+    const [ addNickname, setAddNickname ] = useState('');
+    const changeAddNickname = e => {
+        setAddNickname(e.target.value);
+    };
+
+    const postAddStudent = e => {
+        generalFunctions.axiosInit(axios, token);
+        console.log(addNickname);
+        axios.post('/class/addStudent', {nickname: addNickname});
+    };
 
     return (
         <div>
@@ -60,12 +76,28 @@ function Student(props) {
                 <tr>
                     <th>#</th>
                     <th>닉네임</th>
+                    <th><Button size="sm" onClick={handleShowAddStudent}>새 학생 추가</Button></th>
                 </tr>
                 </thead>
                 <tbody>
                     {tableTemplate}
                 </tbody>
             </Table>
+
+            <Modal show={showAddStudent} onHide={handleCloseAddStudent}>
+                <Modal.Header closeButton>
+                    <Modal.Title>새 학생 추가</Modal.Title>
+                </Modal.Header>
+                <Modal.Body><input type="text" value={addNickname} placeholder="추가할 학생의 닉네임 입력" onChange={changeAddNickname} style={{"width": "100%"}}/></Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseAddStudent}>
+                        취소
+                    </Button>
+                    <Button variant="primary" onClick={postAddStudent}>
+                        추가하기
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
--- react-code/src/component/class/classComponent/StudentInfo.jsx
+++ react-code/src/component/class/classComponent/StudentInfo.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import 'typescript';
 import { useSelector, useDispatch } from 'react-redux';
 import { useCookies } from 'react-cookie';
-import {Button, Card, Table} from 'react-bootstrap';
+import {Card, Col, Container, Row, Table} from 'react-bootstrap';
 import axios from 'axios';
 import config from '../../../config';
 import generalFunctions from "../../../generalFunctions";
@@ -16,24 +16,107 @@ import {
 const setToken = refresh_token => ({ type: "token/SET_TOKEN", refresh_token });
 const toggleLoggedIn = on_off => ({type: config.TOGGLE_LOGGED_IN, on_off});
 
-function StudentInfo() {
+function StudentInfo(props) {
 
+    const dispatch = useDispatch();
+    const [cookies, setCookies, removeCookies] = useCookies(['access_token']);
+
+    const { path, url } = useRouteMatch();
+    const { id } = useParams();
+
+    const token = useSelector(
+        state => state.token
+    );
+    const isLoggedIn = useSelector(
+        state => state.isLoggedIn
+    );
+
+    useEffect(() => {
+        async function get_student_list() {
+            generalFunctions.loggedInTest(axios, cookies, dispatch)
+                .then( res => {
+                    generalFunctions.axiosInit(axios, res.refresh_token);
+
+                })
+        };
+
+    }, [cookies, dispatch]);
+
+    const solvedCardStyle = {
+        margin: "5px 0 0 0",
+        padding: "10px"
+    }
 
     return (
-        <div>
-            <Table striped bordered hover>
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>닉네임</th>
-                </tr>
-                </thead>
-                <tbody>
-
-                </tbody>
-            </Table>
-        </div>
+        <Container>
+            <Row>
+                <Col lg={6} md={12}>
+                    <h3 style={{"margin": "20px 0"}}>과제 목록</h3>
+                    <hr/>
+                    <Table striped bordered hover>
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>과제명</th>
+                            <th>제출일</th>
+                            <th>마감일</th>
+                            <th>진행도</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th>#</th>
+                            <th>과제명</th>
+                            <th>제출일</th>
+                            <th>마감일</th>
+                            <th>진행도</th>
+                        </tr>
+                        <tr>
+                            <th>1</th>
+                            <th>과제1</th>
+                            <th>2019.11.21</th>
+                            <th>2019.11.29</th>
+                            <th>60%</th>
+                        </tr>
+                        <tr>
+                            <th>2</th>
+                            <th>과제2</th>
+                            <th>2019.11.21</th>
+                            <th>2019.11.30</th>
+                            <th>20%</th>
+                        </tr>
+                        </tbody>
+                    </Table>
+                </Col>
+                <Col lg={6} md={12}>
+                    <h3 style={{"margin": "20px 0"}}>OOO 님이 해결한 문제</h3>
+                    <hr/>
+                    <Row>
+                        <Col md={6} sm={12}>
+                            <Card style={solvedCardStyle}>
+                                A+B
+                            </Card>
+                        </Col>
+                        <Col md={6} sm={12}>
+                            <Card style={solvedCardStyle}>
+                                A-B
+                            </Card>
+                        </Col>
+                        <Col md={6} sm={12}>
+                            <Card style={solvedCardStyle}>
+                                A*B
+                            </Card>
+                        </Col>
+                        <Col md={6} sm={12}>
+                            <Card style={solvedCardStyle}>
+                                A/B
+                            </Card>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
-export default StudentInfo;
+export default withRouter(StudentInfo);

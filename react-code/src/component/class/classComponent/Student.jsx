f142fe2ac9a30b60504d75c834755dcebf44328a
qudcksquf <SeoByeongChan@seobyeongchan-ui-MacBook-Pro.local>
2019-11-24 22:26:23 +0900

    modified student add, and delete

--- react-code/src/component/class/classComponent/Student.jsx
+++ react-code/src/component/class/classComponent/Student.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import 'typescript';
 import { useSelector, useDispatch } from 'react-redux';
 import { useCookies } from 'react-cookie';
-import {Button, Modal, Table} from 'react-bootstrap';
+import {Button, Modal, Table, Form} from 'react-bootstrap';
 import axios from 'axios';
 import config from '../../../config';
 import generalFunctions from "../../../generalFunctions";
@@ -59,30 +59,43 @@ function Student(props) {
         generalFunctions.axiosInit(axios, token);
         console.log(addNickname);
         axios.post('/class/addStudentToClass', {
-            id: id,
+            _id: id,
             nickname: addNickname
         }).then(res => {
-            console.log("추가됨");
+            axios.get('/class/getClassUserlist/' + id).then(result => {
+                setUserList(result.data);
+            });
+            alert('추가 되었습니다.');
+        }).catch(err => {
+            alert('추가에 실패했습니다.');
         });
     };
 
     const postDelStudent = e => {
-        generalFunctions.axiosInit(axios, token);
-        const delNickname = e.id;
-        console.log(delNickname);
-        axios.post('/class/deleteStudentInClass/', {
-            id: id,
-            nickname: addNickname
-        }).then(res => {
-            console.log("삭제됨")
-        });
+        const answer = window.confirm(`이 학생을 정말로 삭제하시겠습니까?`);
+        if(answer) {
+            generalFunctions.axiosInit(axios, token);
+
+            const delNickname = e.target.value;
+            axios.post('/class/deleteStudentInClass/', {
+                _id: id,
+                nickname: delNickname
+            }).then(res => {
+                axios.get('/class/getClassUserlist/' + id).then(result => {
+                    setUserList(result.data);
+                });
+                alert('삭제 되었습니다.');
+            }).catch(err => {
+                alert('삭제에 실패했습니다.');
+            });
+        }
     };
 
     const tableTemplate = userList.map((student, i) =>
         <tr key={i + 1}>
             <td>{i + 1}</td>
             <td><Link to={`student/${student.nickname}`}>{student.name}</Link></td>
-            <td><Button onClick={postDelStudent} variant="danger" size="sm">탈퇴</Button></td>
+            <td><Button value={student.nickname} onClick={postDelStudent} variant="danger" size="sm">탈퇴</Button></td>
         </tr>
     );
 
@@ -105,7 +118,7 @@ function Student(props) {
                 <Modal.Header closeButton>
                     <Modal.Title>새 학생 추가</Modal.Title>
                 </Modal.Header>
-                <Modal.Body><input type="text" value={addNickname} placeholder="추가할 학생의 닉네임 입력" onChange={changeAddNickname} style={{"width": "100%"}}/></Modal.Body>
+                <Modal.Body><Form.Control type="text" value={addNickname} placeholder="추가할 학생의 닉네임 입력" onChange={changeAddNickname} style={{"width": "100%"}}/></Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleCloseAddStudent}>
                         취소

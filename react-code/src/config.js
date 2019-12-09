13ab4c0117eb4e42097b95c93d092efe54df6924
qudcksquf <SeoByeongChan@seobyeongchan-ui-MacBook-Pro.local>
2019-12-09 23:44:35 +0900

    api 추가

--- react-code/src/config.js
+++ react-code/src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'serverURL' : 'http://127.0.0.1:3000',
+    'serverURL' : 'http://127.0.0.1:3000/api',
     'TOGGLE_LOGGED_IN': 'isLoggedIn/TOGGLE_LOGGED_IN',
     'CLEAR_TOKEN' : 'token/CLEAR_TOKEN',
     'SET_TOKEN' : 'token/SET_TOKEN',
